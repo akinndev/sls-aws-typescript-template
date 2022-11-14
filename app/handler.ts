16f02cef17ca3ab96bd4ebdb025bc12109ada672
Aki Nguyen <akinguyen@Akis-MacBook-Pro.local>
2022-11-14 10:49:53 +0700

    init source

---
+++ app/handler.ts
@@ -0,0 +1,16 @@
+"use strict";
+import { APIGatewayEvent } from "aws-lambda";
+
+module.exports.hello = async (event: APIGatewayEvent) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: "Go Serverless v3.0! Your function executed successfully!",
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+};
